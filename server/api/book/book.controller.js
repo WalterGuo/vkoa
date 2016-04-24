3b505071a13e0c9870fccebf5c175d02af6e2699
virgo <w2008second@gmail.com>
2016-04-24 20:36:21 +0800

    fixes #1 update project init

--- server/api/book/book.controller.js
+++ server/api/book/book.controller.js
@@ -2,10 +2,13 @@
 
 let Book = require('./book.model');
 
-exports.find = function*(ctx,next){
-  console.log(ctx);
+exports.find = function*(next){
+  console.log(this.body);
   var books = yield Book.find({});
   this.response.body = "hello world!";
+  this.response.body = {
+    msg:"hello world!"
+  };
 
   // this.body = yield this.render('404', {'books': books});
 }
