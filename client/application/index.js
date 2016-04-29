ec7bb2f14b6ece9b062795f9b748797f36e1ce07
virgo <w2008second@gmail.com>
2016-04-30 01:26:39 +0800

    fixes djkkk

--- client/application/index.js
+++ client/application/index.js
@@ -5,7 +5,7 @@ let app = document.createElement('div');
     document.body.appendChild(app);
 export default class App extends React.Component {
   render() {
-    const names = [{date:new Date().getTime(),name:'Jo试试shn'}, {date:new Date().getTime()*10,name:'Jill'}, {date:new Date().getTime()*100,name:'Jack'}];
+    const names = [{date:new Date().getTime(),name:'Jo试试shn'}, {date:new Date().getTime()*10,name:'ggg'}, {date:new Date().getTime()*100,name:'Jajjjjck'}];
 
     return <div>
       <h2>Names</h2>
