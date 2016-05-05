d84fc6e662a8a3c94958d2b263c230ef954e5986
virgo <w2008second@gmail.com>
2016-05-05 20:01:45 +0800

    ff

--- client/app/Note.js
+++ client/app/Note.js
@@ -1,2 +1,19 @@
 import React from 'react';
-export default (props) => <div>{props.task}</div>
+export default class Note extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state ={
+      editing:flase
+    }
+  }
+  render(){
+    if(this.state.editing){
+
+    }
+    return this.renderNote();
+  }
+  renderNote = () => {
+    // If the user clicks a normal note, trigger editing logic.
+    return <div onClick={this.edit}>{this.props.task}</div>;
+  };
+}
